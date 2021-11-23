4accbfd4d8b963081a6191ddd60aca69472373d4
OJAH88 <jharlan628@gmail.com>
2021-11-23 12:08:29 -0500

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,8 +8,28 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
+    plant = find_plant
+    if plant
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+
+  #PATCH /plants/:id
+  def update
     plant = Plant.find_by(id: params[:id])
-    render json: plant
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
   end
 
   # POST /plants
@@ -18,7 +38,18 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def render_not_found_response
+    render json: { error: 'Plant not found' }, status: :not_found
+  end
+
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
